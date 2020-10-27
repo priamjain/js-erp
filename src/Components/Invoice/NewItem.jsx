8745f5dd07edd671d0518ae4a40d274d2804b097
Priam Jain <priamjain8@gmail.com>
2020-10-27 11:49:22 +0530

    Bolded Everything

--- src/Components/Invoice/NewItem.jsx
+++ src/Components/Invoice/NewItem.jsx
@@ -22,7 +22,7 @@ export class NewItem extends React.Component {
 		return (
 			<div className={styles.main} onClick={(e)=>{this.props.setNewItem(false)}}>
 			<form className={styles.form} onSubmit={this.handleSubmit} onClick={e=>e.stopPropagation()}>
-				<p>Client Name: <input type="text" className={styles.input_text} value={this.state.itemName} onChange={e=>this.setState({itemName:e.target.value})} /></p>
+				<p>Product Name: <input type="text" className={styles.input_text} value={this.state.itemName} onChange={e=>this.setState({itemName:e.target.value})} /></p>
 				<div className={styles.buttons}><button className={styles.submit} type="submit">Save</button><div className={styles.cancel} onClick={(e)=>{this.props.setNewItem(false)}}>Cancel</div></div>
 			</form>
 			</div>
